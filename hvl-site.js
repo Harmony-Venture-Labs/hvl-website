21f119d3b5c4b124f4cf692a24e2af548e4f097c
praise <daramolapraise@gmail.com>
2021-12-09 13:18:47 -0600

    added boards

--- hvl-site.js
+++ hvl-site.js
@@ -11,7 +11,7 @@ const addJob = (title, location, url, company) => {
   job.style = `
     cursor: pointer;
   `
-  job.innerHTML = `<a class="list-item">
+  job.innerHTML = `<a style="display: flex; justify-content: space-between;">
     <div style="display: grid;">
       <div class="item-title" style="font-weight: 600;">
         ${title}
@@ -40,17 +40,6 @@ const getJobs = async () => {
   jobs.trustspot.jobs.map(job => {
     addJob(job.title, job.location.name, job.absolute_url, 'TrustSpot');
   });
-  const style = document.createElement('style');
-  style.innerHTML = `
-    .list-item:hover .item-title {
-      text-decoration: underline;
-    }
-
-    .list-item {
-      display: flex;
-      justify-content: space-between;
-    }
-  `;
   document.head.appendChild(style)
   console.log(jobs)
 }
